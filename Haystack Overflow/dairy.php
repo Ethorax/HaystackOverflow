92b4dfdf69d3b2cefbdc73808b6bd283013b4d1c
NicholasKiel <99505374+NicholasKiel@users.noreply.github.com>
2022-03-21 12:44:58 -0400

    Update dairy.php

--- Haystack Overflow/dairy.php
+++ Haystack Overflow/dairy.php
@@ -57,3 +57,4 @@
 </head>
 <h1 style="background-color: rgb(255, 255, 255, 0.6)">Dairy Farming Forums</h1>
 <div style="background-color:rgb(255, 255, 255, 0.6)">Discussing dairy farming till the cows come home!</div>
+?>
