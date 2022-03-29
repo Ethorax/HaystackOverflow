0be0110971bbc1c4ad1abe884551df60ca3dc492
NicholasKiel <99505374+NicholasKiel@users.noreply.github.com>
2022-03-28 20:20:10 -0400

    Update pig.php

--- Haystack Overflow/pig.php
+++ Haystack Overflow/pig.php
@@ -1,4 +1,3 @@
-<?php
 <!DOCTYPE html>
 <html>
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -57,4 +56,3 @@
 </head>
 <h1 style="background-color: rgb(255, 255, 255, 0.6)">Pig and Poultry Forums</h1>
 <div style="background-color:rgb(255, 255, 255, 0.6)">For all your pig and chicken needs.</div>
-?>
