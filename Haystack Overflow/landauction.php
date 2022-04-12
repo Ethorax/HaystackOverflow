b42885c0d17c9ad3cb82e02348f239c3f3aa8be8
NicholasKiel <99505374+NicholasKiel@users.noreply.github.com>
2022-04-12 17:33:50 -0400

    Update landauction.php

--- Haystack Overflow/landauction.php
+++ Haystack Overflow/landauction.php
@@ -29,6 +29,7 @@
         <a href="registration.html">
 			<button>Register</button>
 		  </a>
+	</div>
 </head>
 <h1 style="background-color: rgb(255, 255, 255, 0.6)">Land and Property Auctions Forums</h1>
 <div style="background-color:rgb(255, 255, 255, 0.6)">Going once, Going twice, sold in these forums!</div>
