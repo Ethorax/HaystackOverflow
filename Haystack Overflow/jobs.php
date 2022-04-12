53db4c4c1c52073b7e6bc1088adb58438d44fdee
NicholasKiel <99505374+NicholasKiel@users.noreply.github.com>
2022-04-12 17:33:38 -0400

    Update jobs.php

--- Haystack Overflow/jobs.php
+++ Haystack Overflow/jobs.php
@@ -29,6 +29,7 @@
         <a href="registration.html">
 			<button>Register</button>
 		  </a>
+	</div>
 </head>
 <h1 style="background-color: rgb(255, 255, 255, 0.6)">Jobs Board Forums</h1>
 <div style="background-color:rgb(255, 255, 255, 0.6)">The one stop shop to find or post work.</div>
