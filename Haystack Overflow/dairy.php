c5fa6d955db6a4be166a1c98c18d8844207966ef
NicholasKiel <99505374+NicholasKiel@users.noreply.github.com>
2022-04-12 17:30:26 -0400

    Update dairy.php

--- Haystack Overflow/dairy.php
+++ Haystack Overflow/dairy.php
@@ -48,6 +48,7 @@ if ( mysqli_connect_errno() ) {
         <a href="registration.html">
 			<button>Register</button>
 		  </a>
+	</div>
 </head>
 <h1 style="background-color: rgb(255, 255, 255, 0.6)">Dairy Farming Forums</h1>
 <div style="background-color:rgb(255, 255, 255, 0.6)">Discussing dairy farming till the cows come home!</div>
