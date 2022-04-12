4155d485f39b759edf18ef01638d10ab7b590eee
NicholasKiel <99505374+NicholasKiel@users.noreply.github.com>
2022-04-12 17:32:09 -0400

    Update forage.php

--- Haystack Overflow/forage.php
+++ Haystack Overflow/forage.php
@@ -48,6 +48,7 @@ if ( mysqli_connect_errno() ) {
         <a href="registration.html">
 			<button>Register</button>
 		  </a>
+	</div>
 </head>
 <h1 style="background-color: rgb(255, 255, 255, 0.6)">Livestock and Forage Forums</h1>
 <div style="background-color:rgb(255, 255, 255, 0.6)">Wanna discuss Livestock? How about Foraging? We've got it all here!</div>
