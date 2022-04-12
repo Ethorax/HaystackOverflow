e7c8791ea8e4e2ffd8fc071888a41968bebce7f6
NicholasKiel <99505374+NicholasKiel@users.noreply.github.com>
2022-04-12 17:38:25 -0400

    Update wanted.php

--- Haystack Overflow/wanted.php
+++ Haystack Overflow/wanted.php
@@ -20,9 +20,6 @@
         <a href="Homepage.html">
             <button>Forums</button>
         </a>
-        <a href="chat.html">
-            <button>Chat</button>
-        </a>
         <a href="aboutus.html">
             <button>About Us</button>
         </a>
@@ -32,26 +29,6 @@
         <a href="registration.html">
 			<button>Register</button>
 		  </a>
-        		  <div id="myOverlay" class="overlay">
-			<span class="closebtn" onclick="closeSearch()" title="Close Overlay">×</span>
-			<div class="overlay-content">
-			  <form action="/action_page.php">
-				<input type="text" placeholder="Search.." name="search">
-				<button type="submit"><i class="fa fa-search"></i>Go</button>
-			  </form>
-			</div>
-		  </div>
-		  <button class="openBtn" onclick="openSearch()">Search</button>
-		  
-		  <script>
-		  function openSearch() {
-			document.getElementById("myOverlay").style.display = "block";
-		  }
-		  
-		  function closeSearch() {
-			document.getElementById("myOverlay").style.display = "none";
-		  }
-		  </script>
     </div>
 </head>
 <h1 style="background-color: rgb(255, 255, 255, 0.6)">Wanted Forums</h1>
